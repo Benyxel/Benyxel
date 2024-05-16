d9d004cf1f00441fe6902ad2e81a041fb9a871bd
Yeboah Bernard <yeboahboanubernard1997@gmail.com>
2024-05-16 23:07:28 +0000

    new

--- homeslide.js
+++ homeslide.js
@@ -14,3 +14,25 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// cat.js slide
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+const slideWidth = slides[0].clientWidth;
+let counter = 1;
+
+function slide() {
+  slider.style.transition = 'transform 0.5s ease';
+  slider.style.transform = `translateX(${-slideWidth * counter}px)`;
+  counter++;
+
+  if (counter === slides.length) {
+    setTimeout(() => {
+      slider.style.transition = 'none';
+      slider.style.transform = 'translateX(0)';
+      counter = 1;
+    }, 500);
+  }
+}
+
+setInterval(slide, 5000); // Change slide every 3 seconds
